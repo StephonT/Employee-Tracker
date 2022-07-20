18f66835909d30254a2bd1606eabcbab359cc28d
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-20 17:45:26 -0400

    added new files

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+-->will enable the creation of a new database and only attempt to drop the database if it exists.<--
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+--> Table creations<--
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER 
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id
+);
+
+
+
